32211b5f55eaceb4c7f2266dc1870e3b8314eb79
Aditi Sharma <aditi.sharma04052002@gmail.com>
2024-06-20 15:55:46 +0530

    Trees(BT+BST)

--- DSA_Questions/src/aditi/Trees/Main.java
+++ DSA_Questions/src/aditi/Trees/Main.java
@@ -12,5 +12,13 @@ public class Main {
         tree.root.left.right.right = new Top_View_of_Binary_Tree.Node(5);
         tree.root.left.right.right.right = new Top_View_of_Binary_Tree.Node(6);
         tree.TopView(tree.root);
+//        BottomViewOfBinaryTree.BinaryTree tree = new BottomViewOfBinaryTree.BinaryTree();
+//        tree.root = new BottomViewOfBinaryTree.Node(1);
+//        tree.root.left = new BottomViewOfBinaryTree.Node(2);
+//        tree.root.right = new BottomViewOfBinaryTree.Node(3);
+//        tree.root.left.right = new BottomViewOfBinaryTree.Node(4);
+//        tree.root.left.right.right = new BottomViewOfBinaryTree.Node(5);
+//        tree.root.left.right.right.right = new BottomViewOfBinaryTree.Node(6);
+//        tree.BottomView(tree.root);
         }
     }
